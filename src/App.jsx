a5223456d3728ee9b03fc9ba8d59e4a35166aa55
Declan <imdeckik@gmail.com>
2026-05-09 00:34:55 +1000

    Initial React app setup with Vite

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import { useState } from "react";
+import reactLogo from "./assets/react.svg";
+import viteLogo from "./assets/vite.svg";
+import heroImg from "./assets/hero.png";
+import "./App.css";
+
+function App() {
+  return (
+    <div>
+      <h1>Hello, Declan</h1>
+      <p>My first React component</p>
+    </div>
+  );
+}
+
+export default App;
